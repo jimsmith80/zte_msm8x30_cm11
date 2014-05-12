21c2ef7b8c4becefab5aec705a49e46c96f7522e
jimsmith80 <jimsmith80@icl>
2014-05-12 11:16:53 -0700

    minor update

--- device/zte/warplte/device.mk
+++ device/zte/warplte/device.mk
@@ -24,4 +24,4 @@ DEVICE_PACKAGE_OVERLAYS += device/zte/warplte/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
