a4f981e41bda0111c0fc539bcac52123317a883f
Jim Smith <jimsmith80@icl>
2014-05-22 15:53:46 -0700

    setup base for cdma ril

--- device/zte/warplte/warplte-vendor-blobs.mk
+++ device/zte/warplte/warplte-vendor-blobs.mk
@@ -16,6 +16,36 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/etc/test.txt:system/etc/test.txt
 
-
+# Radio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/bin/mdm_helper:system/bin/mdm_helper \ 
+    $(LOCAL_PATH)/proprietary/bin/ds_fmc_appd:system/bin/ds_fmc_appd \
+    $(LOCAL_PATH)/proprietary/bin/netmgrd:system/bin/netmgrd \
+    $(LOCAL_PATH)/proprietary/bin/qmuxd:system/bin/qmuxd \
+    $(LOCAL_PATH)/proprietary/bin/rild:system/bin/rild \
+    $(LOCAL_PATH)/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    $(LOCAL_PATH)/proprietary/etc/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh \
+    $(LOCAL_PATH)/proprietary/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+    $(LOCAL_PATH)/proprietary/lib/libdiag.so:system/lib/libdiag.so \
+    $(LOCAL_PATH)/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
+    $(LOCAL_PATH)/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
+    $(LOCAL_PATH)/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
+    $(LOCAL_PATH)/proprietary/lib/libomission_avoidance.so:system/lib/libomission_avoidance.so \ *
+    $(LOCAL_PATH)/proprietary/lib/libqdi.so:system/lib/libqdi.so \
+    $(LOCAL_PATH)/proprietary/lib/libqdp.so:system/lib/libqdp.so \
+    $(LOCAL_PATH)/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
+    $(LOCAL_PATH)/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
+    $(LOCAL_PATH)/proprietary/lib/libidl.so:system/lib/libidl.so \
+    $(LOCAL_PATH)/proprietary/lib/libqcci_legacy.so:system/lib/libqcci_legacy.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi.so:system/lib/libqmi.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi_cci.so:system/lib/libqmi_cci.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi_client_qmux.so:system/lib/libqmi_client_qmux.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi_common_so.so:system/lib/libqmi_common_so.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi_csi.so:system/lib/libqmi_csi.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi_csvt_srvc.so:system/lib/libqmi_csvt_srvc.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmi_encdec.so:system/lib/libqmi_encdec.so \
+    $(LOCAL_PATH)/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
+    $(LOCAL_PATH)/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
+    $(LOCAL_PATH)/proprietary/lib/libril.so:system/lib/libril.so
 
 
